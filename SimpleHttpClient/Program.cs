61c1282e16fbb7c03c2ae033525ba30568c0f545
Thomas Schildhauer <tschildhauer@serva-ts.com>
2019-03-04 11:09:20 +0100

    added SimpleHttpClient to Solution

---
+++ SimpleHttpClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace SimpleHttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(() => MainAsync());
+            Console.ReadLine();
+        }
+
+        static async Task MainAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:6740");
+                var content = new FormUrlEncodedContent(new[]
+                {
+                new KeyValuePair<string, string>("", "login")
+            });
+                var result = await client.PostAsync("/api/Membership/exists", content);
+                string resultContent = await result.Content.ReadAsStringAsync();
+                Console.WriteLine(resultContent);
+            }
+        }
+    }
+}
